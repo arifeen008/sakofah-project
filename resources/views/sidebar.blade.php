93f707a8eda7d6b74809b3de87188c373f003ea0
arifeen008 <feenkundee@gmail.com>
2023-12-12 15:26:01 +0700

    Config sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,14 +1,25 @@
 @section('sidebar')
     <div class="mt-3">
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
-        <p><a href="#">Column AAA</a></p>
+        <a href="#"><p class="text-dark">ค้นหาสมาชิก</p></a>
+        <a href="#"><p class="text-dark">สินเชื่อ</p></a>
+        <a href="#"><p class="text-dark">ประกาศภายใน</p></a>
+        <a href="#"><p class="text-dark">ข้อบังคับสหกรณ์</p></a>
+        <a href="#"><p class="text-dark">ระเบียบเจ้าหน้าที่</p></a>
+        <a href="#"><p class="text-dark">แบบฟอร์มเจ้าหน้าที่</p></a>
+        <a href="#"><p class="text-dark">ผลการดำเนินงาน</p></a>
+        <a href="#"><p class="text-dark">อัพโหลดสินเชื่อ</p></a>
+        <a href="#"><p class="text-dark">ฝ่ายสินเชื่อสาขา</p></a>
+        <a href="#"><p class="text-dark">รับเอกสารสินเชื่อสาขา</p></a>
+        <a href="#"><p class="text-dark">ฝ่ายวิเคราะห์</p></a>
+        <a href="#"><p class="text-dark">ผู้จัดการใหญ่</p></a>
+        <a href="#"><p class="text-dark">รายงานผลการติดตามยื่นขอสินเชื่อ</p></a>
+        <a href="#"><p class="text-dark">พิจารณาสินเชื่อ</p></a>
+        <a href="#"><p class="text-dark">สินเชื่อ</p></a>
+        <a href="#"><p class="text-dark">ประวัติการล็อกอิน</p></a>
+        <a href="#"><p class="text-dark">อัพโหลดสินทรัพย์</p></a>
+        <a href="#"><p class="text-dark">อัพโหลดข่าวสาร</p></a>
+        <a href="#"><p class="text-dark">อัพโหลดผลการดำเนินงาน</p></a>
+        <a href="#"><p class="text-dark">อัพโหลดประกาศ</p></a>
+        <a href="#"><p class="text-dark">แก้ไขประกาศ</p></a>
     </div>
 @endsection

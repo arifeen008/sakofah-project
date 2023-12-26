f39f327569e96d7f02a0fdff6708980d6a94ca92
arifeen008 <feenkundee@gmail.com>
2023-12-26 15:44:15 +0700

    ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,10 @@
 @section('content')
     <div id="mainCarousel" class="carousel slide carousel-fade" data-mdb-ride="carousel" data-mdb-carousel-init>
         <div class="carousel-indicators">
-            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="2"
-                aria-label="Slide 3"></button>
+            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-mdb-target="#mainCarousel" data-mdb-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -21,49 +19,31 @@
                 <img src="{{ url('picture/ค่านิยมเว็บ.jpg') }}" class="d-block w-100" />
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-mdb-target="#mainCarousel"
-            data-mdb-slide="prev">
+        <button class="carousel-control-prev" type="button" data-mdb-target="#mainCarousel" data-mdb-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-mdb-target="#mainCarousel"
-            data-mdb-slide="next">
+        <button class="carousel-control-next" type="button" data-mdb-target="#mainCarousel" data-mdb-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
 
     <div class="container mt-4">
-        <div id="secondCarouselBanner" class="carousel slide carousel-dark" data-mdb-ride="secondCarouselBanner">
-            <div class="carousel-indicators">
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="2"
-                    aria-label="Slide 3"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="3"
-                    aria-label="Slide 4"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="4"
-                    aria-label="Slide 5"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="5"
-                    aria-label="Slide 6"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="6"
-                    aria-label="Slide 7"></button>
-                <button type="button" data-mdb-target="#secondCarouselBanner" data-mdb-slide-to="7"
-                    aria-label="Slide 8"></button>
-            </div>
+        <div id="carouselTwo" class="carousel slide" data-mdb-ride="carousel" data-mdb-carousel-init>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ url('picture/548-1.jpg') }}" data-mdb-toggle="modal" data-mdb-target="#Modal2"
-                        class="d-block w-100" />
+                    <img src="{{ url('picture/banner_calendar67.jpg') }}" class="d-block w-100" data-mdb-ripple-init data-mdb-modal-init
+                    data-mdb-target="#Modal4"/>
                 </div>
                 <div class="carousel-item">
-                    <a href="https://shorturl.asia/LGyKT" target="_blank" rel="noopener noreferrer">
-                        <img src="{{ url('picture/521-2.jpg') }}" class="d-block w-100" />
-                    </a>
+                    <img src="{{ url('picture/548-1.jpg') }}" class="d-block w-100" data-mdb-ripple-init data-mdb-modal-init
+                        data-mdb-target="#Modal2" />
                 </div>
-                <div class="carousel-item ">
+                <div class="carousel-item">
+                    <img src="{{ url('picture/521-2.jpg') }}" class="d-block w-100" />
+                </div>
+                <div class="carousel-item">
                     <img src="{{ url('picture/512-3.jpg') }}" class="d-block w-100" />
                 </div>
                 <div class="carousel-item ">
@@ -73,60 +53,72 @@
                     <img src="{{ url('picture/466.jpg') }}" class="d-block w-100" />
                 </div>
                 <div class="carousel-item ">
-                    <img src="{{ url('picture/450.jpg') }}" data-mdb-toggle="modal" data-mdb-target="#Modal0"
-                        class="d-block w-100" />
+                    <img src="{{ url('picture/450.jpg') }}" class="d-block w-100" data-mdb-ripple-init data-mdb-modal-init
+                        data-mdb-target="#Modal0" />
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ url('picture/345-1.jpg') }}" data-mdb-toggle="modal" data-mdb-target="#Modal1"
-                        class="d-block w-100" />
+                    <img src="{{ url('picture/345-1.jpg') }}" class="d-block w-100" data-mdb-ripple-init data-mdb-modal-init
+                        data-mdb-target="#Modal1" />
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ url('picture/520-1.jpg') }}" data-mdb-toggle="modal" data-mdb-target="#Modal3"
-                        class="d-block w-100" />
+                    <img src="{{ url('picture/520-1.jpg') }}" class="d-block w-100" data-mdb-ripple-init
+                        data-mdb-modal-init data-mdb-target="#Modal3" />
                 </div>
-
             </div>
-            <button class="carousel-control-prev" type="button" data-mdb-target="#secondCarouselBanner"
-                data-mdb-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="false"></span>
+            <button class="carousel-control-prev" type="button" data-mdb-target="#carouselTwo" data-mdb-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-mdb-target="#secondCarouselBanner"
-                data-mdb-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="false"></span>
+            <button class="carousel-control-next" type="button" data-mdb-target="#carouselTwo" data-mdb-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-        <div class="modal fade" id="Modal0" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-xl">
+
+        <div class="modal fade modal-lg" id="Modal0" tabindex="-1" aria-labelledby="Modal0" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="ratio ratio-16x9">
+                    <iframe src="https://www.youtube.com/embed/Wjw0Eovdgg0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade modal-lg" id="Modal1" tabindex="-1" aria-labelledby="Modal1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-body">
-                        <div class="ratio ratio-16x9">
-                            <iframe src="https://www.youtube.com/embed/Wjw0Eovdgg0" title="YouTube video"
-                                allowfullscreen></iframe>
-                        </div>
+                        <img src="{{ url('picture/345.jpg') }}" class="img-fluid">
                     </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="Modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
+
+        <div class="modal fade modal-lg" id="Modal2" tabindex="-1" aria-labelledby="Modal2" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                    <div class="modal-body"><img src="{{ url('picture/345.jpg') }}" class="img-fluid"></div>
+                    <div class="modal-body">
+                        <img src="{{ url('picture/548.jpg') }}" class="img-fluid">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="Modal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
+
+        <div class="modal fade modal-lg" id="Modal3" tabindex="-1" aria-labelledby="Modal3" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                    <div class="modal-body"><img src="{{ url('picture/548.jpg') }}" class="img-fluid"></div>
+                    <div class="modal-body">
+                        <img src="{{ url('picture/520.jpg') }}" class="img-fluid">
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="modal fade" id="Modal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
+
+        <div class="modal fade modal-lg" id="Modal4" tabindex="-1" aria-labelledby="Modal4" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                    <div class="modal-body"><img src="{{ url('picture/520.jpg') }}" class="img-fluid"></div>
+                    <div class="modal-body">
+                        <img src="{{ url('picture/detail_banner_calendar67.jpg') }}" class="img-fluid">
+                    </div>
                 </div>
             </div>
         </div>

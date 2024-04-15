309f9905646c5a8ee5b7a6f66690536f6b72cd72
arifeen008 <feenkundee@gmail.com>
2024-04-15 09:32:05 +0700

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -461,27 +461,5 @@
             </a>
         </div>
     </div>
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
-        data-mdb-backdrop="false" data-mdb-keyboard="false">
-        <div class="modal-dialog   modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <img src="{{ url('banner/time.jpg') }}" class="w-100" alt="time">
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
 @endsection
-@section('script')
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#exampleModal').modal('show');
-        });
-    </script>
-@endsection
+
